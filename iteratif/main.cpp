5b46a3e3b742f64edd0cab1a8857d5a55d329ef2
Veiron Vaya Yarief <veironvayayarief41@gmail.com>
2024-12-25 17:54:19 +0700

    Update main.cpp

--- iteratif/main.cpp
+++ iteratif/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    void heapifyDownIterative(int arr[], int n, int i) {
-    while (true) {
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
+
+void heapifyDownIterative(int arr[], int n, int i) {
+    for (int k = n / 2 - 1; k >= 0; k--) {
+        int largest = k;
+        int left = 2 * k + 1;
+        int right = 2 * k + 2;
 
         if (left < n && arr[left] > arr[largest])
             largest = left;
@@ -16,14 +14,42 @@ int main()
         if (right < n && arr[right] > arr[largest])
             largest = right;
 
-        if (largest != i) {
-            std::swap(arr[i], arr[largest]);
-            i = largest;
-        } else {
-            break;
+        if (largest != k) {
+            swap(arr[k], arr[largest]);
+
+            int i = largest;
+            while (i < n) {
+                largest = i;
+                left = 2 * i + 1;
+                right = 2 * i + 2;
+
+                if (left < n && arr[left] > arr[largest])
+                    largest = left;
+
+                if (right < n && arr[right] > arr[largest])
+                    largest = right;
+
+                if (largest != i) {
+                    swap(arr[i], arr[largest]);
+                    i = largest;
+                } else {
+                    break;
+                }
+            }
         }
     }
 }
+
+int main() {
+    int arr[] = {1,2,3,4,5,6,7,8,9,10};
+    int n = 10;
+
+    heapifyDownIterative(arr,n,(n/2)-1);
+
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
 
     return 0;
 }
